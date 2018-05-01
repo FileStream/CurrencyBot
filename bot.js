6ff95c8d7ee0368c28ba133ff41d8226e020de8a
FileStream <jpahman2003@gmail.com>
2018-05-01 17:53:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ bot.on('ready', function(evt) {
      for (var times of userData[u].expireTimes) {
        if (!items[times['item']]) continue;
       if ((times['date']+(items[times['item']].expireTime*60000))<=now) {
-        userData[u].purchasedItems.splice(u.purchasedItems.indexOf(userData[u].expireTimes.find(t=>t.item=times['item'])),0);
+        userData[u].purchasedItems.splice(userData[u].purchasedItems.indexOf(userData[u].expireTimes.find(t=>t.item=times['item'])),0);
        bot.sendMessage({to:u,message:"Your " + items[times['item']].displayData['name'] + " upgrade has expired. Buy a new one!"}); 
       }
        else console.log(u + "'s upgrade is ok");
