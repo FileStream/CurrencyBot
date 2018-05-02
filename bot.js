865fb2fefe2d5df964f1e59b754236f7975f602c
FileStream <jpahman2003@gmail.com>
2018-05-02 11:37:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,7 +144,7 @@ function addPoint(channelID, userID, amount) {
  if (amount===undefined) amount=1;
   let itemList = userData[userID].purchasedItems;
   for (let i of Object.keys(items)) {
-   if (itemList.includes(i)) {
+   if (Object.values(itemList).includes(i)) {
     amount = items[i].function([channelID,userID,amount]);
     if (itemList.find(n=>n.item==i).uses==1)
       itemList.splice(itemList.indexOf(itemList.find(it=>it.item==i)),1);
