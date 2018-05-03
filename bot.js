183213c574c33954f75bc3c17036ae96df1388a7
FileStream <jpahman2003@gmail.com>
2018-05-03 12:32:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,7 +357,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
          }
             break;
           case 'items':
-          let Sstring = "";
+          var Sstring = "";
             let c = 1;
             for (let i of Object.keys(items).find(ite=>ite.uses!=0)) {
               Sstring+= c + ': **'+items[i].displayData.name+'**\n   *' + items[i].displayData.description + '*\nUses left: **' + userData[userID].purchasedItems.find(u=>u.item==i).uses + '**\n\n';
