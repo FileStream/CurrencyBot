158634f33bce2fe10703591d768b7369d21c4359
FileStream <jpahman2003@gmail.com>
2018-05-04 08:49:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var isDeaf = false;
 var pageHolder = [];
 var lastMsg;
 var userData = {};
-const bigInteger = require('./javascript-biginteger-master/BigInteger.js').BigInteger;
+const bigInteger = require('./javascript-biginteger-master').BigInteger;
 var items = {
   'times5': {
   'expireTime': 60, //time from buying to expire in minutes
