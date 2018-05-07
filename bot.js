23a70e592f3aec7f5406f18d69f406adcf521f79
FileStream <jpahman2003@gmail.com>
2018-05-07 10:38:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,7 +219,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
             case 'ping':
                 var date = new Date(Date.now());
                 var currentTime = date.getMilliseconds();
-                var msgTime = (parseInt(evt.d.timestamp.split('.')[0].split(':')[2]) * 60000) + parseInt(evt.d.timestamp.split('.')[1].substring(0, 3));
+                var msgTime = currentTime - (parseInt(evt.d.timestamp.split('.')[0].substring(0, -3))*1000+parseInt(evt.d.timestamp.split('.')[0].substring(0,3)))
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong! `' + (currentTime - msgTime) + ' ms`'
