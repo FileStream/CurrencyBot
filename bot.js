622f769ccecb4da1ffc1f7277b83ce5c4c3eebc0
Bin <jpahman2003@gmail.com>
2018-08-02 03:34:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -343,7 +343,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
               let vchannels = Object.values(bot.servers['428702206078746634'].channels).filter(x=>x.type!='text');
               for (var c of Object.values(vchannels)) {
                console.log("Voice channel name: " + c.name);
-                console.log("Current occupants: " + c);
+                console.log("Current occupants: " + c.toString());
               }
             }
             break;
