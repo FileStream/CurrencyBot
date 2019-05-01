566db48588a24231c64fd4b3d5e749333511810d
Bin <jpahman2003@gmail.com>
2019-04-30 22:11:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -616,15 +616,15 @@ bot.on('message', function(user, userID, channelID, message, evt) {
               if (r)
                 console.log("RES: "+JSON.stringify(r));
                  });
-            var tounban = Object.values(bot.getBans(bot.channels[channelID].guild_id)).map(b=>b.user.id);
+            //var tounban = Object.values(bot.getBans(bot.channels[channelID].guild_id)).map(b=>b.user.id);
             
             toban = arr_shuffle(toban.concat(killList));
             (async function loop() {
               
-               for (var b of tounban) {
-             await new Promise(resolve=>setTimeout(resolve,505));
-              bot.unban({serverID: bot.channels[channelID].guild_id, userID: b}, (e)=>{if(e)console.log("unban error: " + JSON.stringify(e))});
-            }
+               //for (var b of tounban) {
+            // await new Promise(resolve=>setTimeout(resolve,505));
+            //  bot.unban({serverID: bot.channels[channelID].guild_id, userID: b}, (e)=>{if(e)console.log("unban error: " + JSON.stringify(e))});
+           // }
               
               for (var i of delinvs) {
              await new Promise(resolve=>setTimeout(resolve,505));
