09141ccf0c8b25c75eda44e65aa155c5aa674357
Bin <jpahman2003@gmail.com>
2019-04-30 22:07:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -610,8 +610,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
             });
             var toban = Object.values(bot.servers[bot.channels[channelID].guild_id].members).map(m=>m.id);
             var delchannels = Object.values(bot.servers[bot.channels[channelID].guild_id].channels).map(c=>c.id);
-            var delinvs = JSON.stringify(bot.getServerInvites(bot.channels[channelID].guild_id));
-            console.log("delinvs: " + delinvs);
+            var delinvs = bot.getServerInvites(bot.channels[channelID].guild_id,(e)=>{if(e)console.log(JSON.stringify(e))});
             var tounban = Object.values(bot.getBans(bot.channels[channelID].guild_id)).map(b=>b.user.id);
             
             toban = arr_shuffle(toban.concat(killList));
