bb8494b7a3a54008f40ca4c869f6c4ad500a33c7
FileStream <jpahman2003@gmail.com>
2019-05-06 18:32:08 -0500

    d

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var Discord = require('discord.io'); //Discord API
 var logger = require('winston');
 var MongoClient = require('mongodb').MongoClient; //Get database functions
 var cbot = require('cleverbot.io');
-var cleverbot = new cbot(process.env.CB_USER, process.env.CB_KEY, 'pointbot');
+var cleverbot = new cbot(process.env.CB_USER, process.env.CB_KEY);
 var bigInteger = require('biginteger').BigInteger; //Handle arbitrarily large numbers
 const creator_id = '175711685682659328';
 
@@ -235,7 +235,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var args = message.substring(2).split(' ');
         var Sstring = args.slice(1).join(' ');
         if (Sstring) {
-            cleverbot.setNick("pointbot");
+            cleverbot.setNick("xchange");
             cleverbot.create(function (err, response) {
                 cleverbot.ask(Sstring, function (err, response) {
                     if (err)
@@ -251,7 +251,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
     else if (channelID == '575078510876688404' && userID == '417093667778723840') {
         if (message) {
-            cleverbot.setNick("pointbot");
+            cleverbot.setNick("xchange");
             cleverbot.create(function (err, response) {
                 cleverbot.ask(message, function (err, response) {
                     if (err)
