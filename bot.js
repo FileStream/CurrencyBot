cfab8ec00fb47566d53729edda8d34800852cbb1
FileStream <jpahman2003@gmail.com>
2019-05-06 21:49:46 -0500

    dssad

--- bot.js
+++ bot.js
@@ -276,7 +276,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
     else if (channelID == '575112665983352835' && userID == '417093667778723840') {
         if (message) {
-            if (Math.floor(Math.random() * 1001) == 1000) bot.pinMessage({
+            if (Math.floor(Math.random() * 1000) == 999) bot.pinMessage({
                 channelID: channelID,
                 messageID: evt.d.id
             });
